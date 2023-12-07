40bc4504e5d71f428b1fd9ded598299463bbeda3
Michael Spitznagel <exultavit@gmail.com>
2023-12-07 14:47:47 -0500

    Solve day 04 part one

--- 04/day04.py
+++ 04/day04.py
@@ -1,12 +1,20 @@
 """Solves day 04, Advent of Code 2023."""
 
 from aocd.models import Puzzle
+from parse import parse
 
+def parse_line(line: str) -> tuple:
+    card, win_str, pick_str = parse('{}:{}|{}',line)
+    wins = set(int(win) for win in win_str.split())
+    picks = set(int(pick) for pick in pick_str.split())
+    return (card, wins, picks)
 
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
-
-    return "Solution not implemented"
+    games = [parse_line(line) for line in input_data.split('\n')]
+    matches = [len(set.intersection(wins,picks)) for card, wins, picks in games]
+    scores = [0 if x == 0 else pow(2,x-1) for x in matches]
+    return sum(scores)
 
 
 def part_b(input_data: str) -> int:
