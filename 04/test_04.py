b29a1a87249b7cbf12f041fd10cd709d09ef7c07
Michael Spitznagel <exultavit@gmail.com>
2023-12-07 14:10:26 -0500

    Provision day 04

---
+++ 04/test_04.py
@@ -0,0 +1,20 @@
+"""Tests for day 04 of Advent of Code 2023."""
+
+import day04
+
+# Test data given as a multiline string.
+sample_input_data = """Sample data not entered"""
+
+sample_solution_a = "Sample solution not entered"
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day04.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day04.part_b(sample_input_data) == sample_solution_b
