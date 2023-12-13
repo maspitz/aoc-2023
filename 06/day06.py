7654f012e47e9b15b685550ee60b3b1a8b634295
Michael Spitznagel <exultavit@gmail.com>
2023-12-13 14:17:44 -0500

    Provision day 06

---
+++ 06/day06.py
@@ -0,0 +1,23 @@
+"""Solves day 06, Advent of Code 2023."""
+
+from aocd.models import Puzzle
+
+
+def part_a(input_data: str) -> int:
+    """Given the puzzle input data, return the solution for part A."""
+
+    return "Solution not implemented"
+
+
+def part_b(input_data: str) -> int:
+    """Given the puzzle input data, return the solution for part B."""
+
+    return "Solution not implemented"
+
+
+if __name__ == '__main__':
+    puzzle = Puzzle(year=2023, day=6)
+
+    print(f"Puzzle {puzzle.year}-12-{puzzle.day:02d}: {puzzle.title}")
+    print(f"  Part A: {part_a(puzzle.input_data)}")
+    print(f"  Part B: {part_b(puzzle.input_data)}")
---
+++ 06/test_06.py
@@ -0,0 +1,20 @@
+"""Tests for day 06 of Advent of Code 2023."""
+
+import day06
+
+# Test data given as a multiline string.
+sample_input_data = """Sample data not entered"""
+
+sample_solution_a = "Sample solution not entered"
+
+sample_solution_b = "Sample solution not entered"
+
+
+def test_part_a():
+    """Test the solution on sample data for part A."""
+    assert day06.part_a(sample_input_data) == sample_solution_a
+
+
+def test_part_b():
+    """Test the solution on sample data for part B."""
+    assert day06.part_b(sample_input_data) == sample_solution_b
