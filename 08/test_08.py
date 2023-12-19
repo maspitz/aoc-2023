aa4df56aec5ea1eab191793c6523d6311d7eb9e5
Michael Spitznagel <exultavit@gmail.com>
2023-12-19 14:59:07 -0500

    Add tests for part one day 08

--- 08/test_08.py
+++ 08/test_08.py
@@ -3,16 +3,33 @@
 import day08
 
 # Test data given as a multiline string.
-sample_input_data = """Sample data not entered"""
+sample_input_data_1 = """RL
 
-sample_solution_a = "Sample solution not entered"
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)"""
+
+sample_solution_a_1 = 2
+
+sample_input_data_2 = """LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"""
+
+sample_solution_a_2 = 6
 
 sample_solution_b = "Sample solution not entered"
 
 
 def test_part_a():
     """Test the solution on sample data for part A."""
-    assert day08.part_a(sample_input_data) == sample_solution_a
+    assert day08.part_a(sample_input_data_1) == sample_solution_a_2
+    assert day08.part_a(sample_input_data_2) == sample_solution_a_2
 
 
 def test_part_b():
