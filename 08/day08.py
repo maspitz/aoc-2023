d3cac84eab9d7ec7e1ff5dda26cf9162375568ad
Michael Spitznagel <exultavit@gmail.com>
2023-12-19 15:15:44 -0500

    Solve part one day 08

--- 08/day08.py
+++ 08/day08.py
@@ -1,12 +1,50 @@
 """Solves day 08, Advent of Code 2023."""
 
 from aocd.models import Puzzle
+from parse import parse
+
+
+
+def parse_input(input_data: str) -> tuple[str, dict]:
+    lines = input_data.splitlines()
+    instructions = lines[0]
+    network_lines = [parse("{} = ({}, {})", line).fixed for line in lines[2:]]
+
+    network = {node: (left, right)
+               for node, left, right in network_lines}
+    return instructions, network
+
+
+def apply_steps(location: str, target: str, instructions: str, network: dict) -> tuple[int, str]:
+    """Return the number of steps taken and the destination reached in those
+    steps according to the given arguments.
+
+    The number of steps will be equal to either the steps until the target is reached,
+    or the number of instructions, whichever is smaller."""
+
+    steps = 0
+    for direction in instructions:
+        if location == target:
+            return (steps, location)
+        steps += 1
+        if direction == 'L':
+            location = network[location][0]
+        else:
+            location = network[location][1]
+    return (steps, location)
 
 
 def part_a(input_data: str) -> int:
     """Given the puzzle input data, return the solution for part A."""
 
-    return "Solution not implemented"
+    inst, net = parse_input(input_data)
+    total_steps = 0
+    location = 'AAA'
+    target = 'ZZZ'
+    while target != location:
+        new_steps, location = apply_steps(location, target, inst, net)
+        total_steps += new_steps
+    return total_steps
 
 
 def part_b(input_data: str) -> int:
